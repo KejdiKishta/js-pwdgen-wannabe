5dd07985730cd4d77fb9644a0692c81713489fec
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-03-22 15:36:17 +0100

    add: bonus random number

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,36 @@
 //TEST
 // alert("Ciao");
 
-// Chiedi all’utente il suo nome
+// !Chiedi all’utente il suo nome
 
 let userName = prompt("Ciao! Come ti chiami?"); //*string
 console.log(userName, typeof userName);
 
-// Poi chiedi il suo cognome
+// !Poi chiedi il suo cognome
 
 let userSurname = prompt("Bene! E il tuo cognome qual è?") //*string
 console.log(userSurname, typeof userSurname);
 
-// Poi chiedi il suo colore preferito
+// !Poi chiedi il suo colore preferito
 
 let userColor = prompt("Ottimo! Ora mi sapresti dire il tuo colore preferito?") //*string
 console.log(userColor, typeof userColor);
 
-// Crea una const con la somma delle variabili precendenti
+// !Bonus numero casuale da 1 a 99
+
+// let randomNumber = Math.random() * 100
+
+// Un numero casuale intero tra uno e 99
+
+let randomNumber = Math.floor(Math.random() * 100);
+
+// !Crea una const con la somma delle variabili precendenti
 
 // const newPass = userName + userSurname + userColor + "21" //*string con operatore +
-const newPass = `${userName}${userSurname}${userColor}21` //* string con template literal
+// const newPass = `${userName}${userSurname}${userColor}21` //* string con template literal
+const newPass = userName + userSurname + userColor + randomNumber
 console.log(newPass, typeof newPass);
 
-// Infine scrivi sulla pagina nomecognomecolorepreferito21
+// !Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-document.getElementById("result").innerHTML = newPass
+document.getElementById("result").innerHTML = newPass;
